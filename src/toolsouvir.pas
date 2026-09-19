a903bfd521b0997468203f3cea85c5bd99119480
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 11:55:10 -0300

    refactor: moderniza cliente TCP Lazarus

--- src/toolsouvir.pas
+++ src/toolsouvir.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  AdvLed, lNetComponents, lNet, strutils,  Process, rotulo, texto;
+  AdvLed, lNetComponents, lNet, fpjson, jsonparser, rotulo, texto;
 
 type
 
@@ -23,7 +23,6 @@ type
     Label2: TLabel;
     Label3: TLabel;
     LTCPComponent1: TLTCPComponent;
-    LTCPComponent2: TLTCPComponent;
     Shape1: TShape;
     procedure btConectClick(Sender: TObject);
     procedure btDisconectClick(Sender: TObject);
@@ -31,18 +30,16 @@ type
     procedure LTCPComponent1Connect(aSocket: TLSocket);
     procedure LTCPComponent1Disconnect(aSocket: TLSocket);
     procedure LTCPComponent1Receive(aSocket: TLSocket);
-    procedure LTCPComponent2Connect(aSocket: TLSocket);
-    procedure LTCPComponent2Disconnect(aSocket: TLSocket);
-    procedure LTCPComponent2Receive(aSocket: TLSocket);
     procedure Shape1ChangeBounds(Sender: TObject);
   private
-    lastfrase : string;
+    FReceiveBuffer: string;
+    FLastPhrase: string;
+    procedure ProcessLine(const ALine: string);
+    function ExtractCaptionText(const ALine: string): string;
   public
-    frase : string;
-    procedure Conectar();
-    procedure Disconectar();
-    procedure EnviaPerguntaAssociada(texto : String);
-    procedure ProcessarBatch();
+    frase: string;
+    procedure Conectar;
+    procedure Disconectar;
   end;
 
 var
@@ -52,174 +49,127 @@ implementation
 
 {$R *.lfm}
 
-uses main;
-
 { TfrmToolsOuvir }
-
 
 procedure TfrmToolsOuvir.Shape1ChangeBounds(Sender: TObject);
 begin
-
 end;
 
 procedure TfrmToolsOuvir.btConectClick(Sender: TObject);
 begin
-   Conectar();
+  Conectar;
 end;
 
 procedure TfrmToolsOuvir.btDisconectClick(Sender: TObject);
 begin
-  Disconectar();
+  Disconectar;
 end;
 
 procedure TfrmToolsOuvir.LTCPComponent1Accept(aSocket: TLSocket);
 begin
-
-
 end;
 
 procedure TfrmToolsOuvir.LTCPComponent1Connect(aSocket: TLSocket);
 begin
-  AdvLed1.Blink:=false;
-  AdvLed1.State:= lsOn;
-
+  AdvLed1.Blink := False;
+  AdvLed1.State := lsOn;
 end;
 
 procedure TfrmToolsOuvir.LTCPComponent1Disconnect(aSocket: TLSocket);
 begin
-  AdvLed1.Blink:=false;
-  AdvLed1.State:= lsOff;
+  AdvLed1.Blink := False;
+  AdvLed1.State := lsOff;
+end;
+
+function TfrmToolsOuvir.ExtractCaptionText(const ALine: string): string;
+var
+  Data: TJSONData;
+  Obj: TJSONObject;
+begin
+  Result := '';
+  if Trim(ALine) = '' then
+    Exit;
+
+  { Compatibilidade: servidores antigos ainda podem enviar texto puro. }
+  if TrimLeft(ALine)[1] <> '{' then
+  begin
+    Result := Trim(ALine);
+    Exit;
+  end;
+
+  Data := nil;
+  try
+    Data := GetJSON(ALine);
+    if Data.JSONType <> jtObject then
+      Exit;
+
+    Obj := TJSONObject(Data);
+    if Obj.Get('type', '') = 'caption' then
+      Result := Obj.Get('text', '');
+  except
+    on E: Exception do
+      Result := '';
+  end;
+  Data.Free;
+end;
+
+procedure TfrmToolsOuvir.ProcessLine(const ALine: string);
+var
+  Info: string;
+begin
+  Info := ExtractCaptionText(ALine);
+  if Info = '' then
+    Exit;
+
+  edTexto.Text := Info;
+
+  if FLastPhrase = Info then
+    Exit;
+
+  FLastPhrase := Info;
+  frase := Info;
+
+  if Assigned(frmrotulo) then
+    frmrotulo.Label1.Caption := Info;
+
+  if Assigned(frmtexto) then
+    frmtexto.Adicionar(Info);
 end;
 
 procedure TfrmToolsOuvir.LTCPComponent1Receive(aSocket: TLSocket);
 var
-   info : String;
-   posicao : integer;
+  Chunk: string;
+  P: SizeInt;
+  Line: string;
 begin
-  //ShowMessage('Recebeu a mensagem:');
-  info := '';
-  aSocket.GetMessage(info);
-  frase := 'doctor';
-  //posicao := pos(lowercase(frase),lowercase(info));
+  Chunk := '';
+  aSocket.GetMessage(Chunk);
+  FReceiveBuffer := FReceiveBuffer + Chunk;
 
-  edTexto.Text := lowercase(info);
-  if(true) then
-  begin
-       if(lastfrase <> info) then
-       begin
-         lastfrase := info;
-         info := replacestr(info,frase, '');
-         frmrotulo.Label1.Caption:=info;
-         frmtexto.Adicionar(info);
-         Application.ProcessMessages;
-         //frmToolsfalar.falar('Claro, deixa eu ver se encontro sua resposta!');
-         //frmmain.NewContext();
-         //frmmain.pergunta := info;
-         //ProcessarBatch();
-         //frmmain.FazPergunta();
-         //Registra a frase que veio
-         //frmmain.RecebeuPergunta(info);
+  repeat
+    P := Pos(#10, FReceiveBuffer);
+    if P = 0 then
+      Break;
 
-         //Solicita verificação da associação do texto ao servico python
-         //EnviaPerguntaAssociada(info);
-       end;
-
-
-  end;
-
-  //ShowMessage(info);
+    Line := Copy(FReceiveBuffer, 1, P - 1);
+    Delete(FReceiveBuffer, 1, P);
+    Line := StringReplace(Line, #13, '', [rfReplaceAll]);
+    ProcessLine(Line);
+  until False;
 end;
 
-procedure TfrmToolsOuvir.LTCPComponent2Connect(aSocket: TLSocket);
+procedure TfrmToolsOuvir.Conectar;
 begin
+  if LTCPComponent1.Active then
+    Exit;
 
+  LTCPComponent1.Connect(edIP.Text, StrToIntDef(edPort.Text, 8097));
 end;
 
-procedure TfrmToolsOuvir.LTCPComponent2Disconnect(aSocket: TLSocket);
+procedure TfrmToolsOuvir.Disconectar;
 begin
-
-end;
-
-procedure TfrmToolsOuvir.LTCPComponent2Receive(aSocket: TLSocket);
-var
-   info : String;
-   posicao : integer;
-begin
-  //ShowMessage('Recebeu a mensagem:');
-  info := '';
-  aSocket.GetMessage(info);
-
-
-  //edTexto.Text := lowercase(info);
-  if(info<>'-1') then
-  begin
-    //frmToolsfalar.Falar('Encontrei uma resposta');
-    //dmbanco.RecebeuAssociacao(info );
-  end;
-
-  //ShowMessage(info);
-end;
-
-procedure TfrmToolsOuvir.Conectar();
-begin
-  LTCPComponent1.Connect(edIP.text,strtoint(edPort.text));
-  LTCPComponent2.Connect(edIP.text,strtoint(edPort.text)+1);
-  if(not LTCPComponent1.Active) then
-  begin
-       //frmToolsfalar.Falar('Não foi possível conectar no serviço de escutar voz');
-  end;
-  if(not LTCPComponent2.Active) then
-  begin
-       //frmToolsfalar.Falar('Não foi possível conectar no serviço de processar voz');
-  end;
-
-end;
-
-procedure TfrmToolsOuvir.Disconectar();
-begin
-  LTCPComponent1.Disconnect(true);
-  LTCPComponent2.Disconnect(true);
-end;
-
-procedure TfrmToolsOuvir.EnviaPerguntaAssociada(texto: String);
-begin
- LTCPComponent2.SendMessage(texto, nil);
-end;
-
-procedure TfrmToolsOuvir.ProcessarBatch();
-var
-  ScriptPath: String;
-  AProcess: TProcess;
-begin
-  AProcess := TProcess.Create(nil);
-  try
-    // Detecta o sistema operacional
-    {$IFDEF WINDOWS}
-    ScriptPath := 'D:\projetos\maurinsoft\doctor\software\script\scripts.bat';
-    {$ELSE}
-    ScriptPath := '/path/to/your/linux/scripts.sh'; // Substitua pelo caminho correto no Linux
-    {$ENDIF}
-
-    // Configura o processo para executar o script
-    AProcess.Executable := ScriptPath;
-
-    {$IFDEF UNIX}
-    // Necessário para executar shell scripts no Linux
-    AProcess.Executable := '/bin/bash';
-    AProcess.Parameters.Add(ScriptPath);
-    {$ENDIF}
-
-    AProcess.Options := [poWaitOnExit]; // Aguarda a execução ser concluída
-    AProcess.Execute;
-
-    // Exibe uma mensagem indicando sucesso
-    //ShowMessage('Script executado com sucesso!');
-  except
-    on E: Exception do
-      //ShowMessage('Erro ao executar o script: ' + E.Message);
-  end;
-  AProcess.Free;
+  if LTCPComponent1.Active then
+    LTCPComponent1.Disconnect(True);
 end;
 
 end.
-
